2c8d78eeb91a23ca336bdaa3f9966ff49e56b089
Iris <iris.devillars@gmail.com>
2024-06-18 11:56:19 +0200

    fix: get deployment data of connected wallet

--- hooks/isDeployed.tsx
+++ hooks/isDeployed.tsx
@@ -1,10 +1,14 @@
 import { GetDeploymentDataResult, NetworkType } from "@/constants/types";
+import { useConnect } from "@starknet-react/core";
 import { useEffect, useState } from "react";
+import { wallet } from "starknet";
+import getStarknet, { StarknetWindowObject } from "get-starknet-core";
 
 export default function isStarknetDeployed(
   network?: NetworkType,
   address?: string
 ) {
+  const { connector } = useConnect();
   const [deploymentData, setDeploymentData] =
     useState<GetDeploymentDataResult>();
 
@@ -16,22 +20,34 @@ export default function isStarknetDeployed(
 
     const checkIsDeployed = async () => {
       try {
-        if (!window.starknet || !window.starknet.isConnected) {
+        const availableWallets = await getStarknet.getAvailableWallets();
+
+        if (!availableWallets) {
           setDeploymentData(undefined);
           return;
         }
 
-        const data = await window.starknet?.request({
-          // @ts-ignore
-          type: "wallet_deploymentData",
+        availableWallets.forEach(async (connectedWallet) => {
+          if (
+            connectedWallet.id === connector?.id &&
+            connectedWallet.isConnected
+          ) {
+            const data = await wallet.deploymentData(
+              // @ts-ignore
+              connectedWallet as StarknetWindowObject
+            );
+            console.log("Starknet data for deployment", data);
+            if (isGetDeploymentDataResult(data)) {
+              setDeploymentData(data);
+            } else {
+              console.error(
+                "Received data is not in the expected format:",
+                data
+              );
+              setDeploymentData(undefined);
+            }
+          }
         });
-        console.log("Starknet data for deployment", data);
-        if (isGetDeploymentDataResult(data)) {
-          setDeploymentData(data);
-        } else {
-          console.error("Received data is not in the expected format:", data);
-          setDeploymentData(undefined);
-        }
       } catch (error) {
         setDeploymentData(undefined);
       }
