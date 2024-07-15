84003c67b0db8f899cad5c9daa48968d82fad60a
Iris <iris.devillars@gmail.com>
2024-07-15 14:47:58 +0200

    fix: typing error

--- app/components/videoBackground.tsx
+++ app/components/videoBackground.tsx
@@ -12,13 +12,13 @@ const VideoBackground = () => {
   );
 };
 
-const videoStyle: any = {
+const videoStyle: {} = {
   width: "100%",
   height: "100%",
   objectFit: "cover",
 };
 
-const containerStyle: any = {
+const containerStyle: {} = {
   position: "fixed",
   right: "0",
   bottom: "0",
@@ -28,7 +28,7 @@ const containerStyle: any = {
   overflow: "hidden",
 };
 
-const overlayStyle: any = {
+const overlayStyle: {} = {
   position: "absolute",
   top: "0",
   left: "0",
