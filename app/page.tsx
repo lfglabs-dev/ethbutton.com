8b02d83e65b68f12557100506cec76e6cc34440c
Iris <iris.devillars@gmail.com>
2024-09-19 08:57:18 +0200

    fix: constant condition

--- app/page.tsx
+++ app/page.tsx
@@ -110,6 +110,7 @@ export default function Home() {
   const [showErrorMsg, setShowErrorMsg] = useState<boolean>(false);
   const [errorMsg, setErrorMsg] = useState<string>("");
   // const [leaderboard, setLeaderboard] = useState<boolean>(false);
+  const leaderboard = true;
   const [maintenance, setMaintenance] = useState<boolean>(false);
   const unexpectedError = isUnexpectedError();
 
@@ -653,7 +654,7 @@ export default function Home() {
 
   return (
     <>
-      {true ? (
+      {leaderboard ? (
         <LeaderboardWrapper />
       ) : maintenance ? (
         <Maintenance
