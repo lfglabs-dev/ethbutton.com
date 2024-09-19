fcbc3f0880db152d59e6a93a5403b9a84b16cfbf
Iris <iris.devillars@gmail.com>
2024-09-19 08:55:30 +0200

    fix: unused variables

--- app/page.tsx
+++ app/page.tsx
@@ -109,7 +109,7 @@ export default function Home() {
   const [showNotifPlayed, setShowNotifPlayed] = useState<boolean>(false);
   const [showErrorMsg, setShowErrorMsg] = useState<boolean>(false);
   const [errorMsg, setErrorMsg] = useState<string>("");
-  const [_leaderboard, setLeaderboard] = useState<boolean>(false);
+  // const [leaderboard, setLeaderboard] = useState<boolean>(false);
   const [maintenance, setMaintenance] = useState<boolean>(false);
   const unexpectedError = isUnexpectedError();
 
@@ -185,7 +185,7 @@ export default function Home() {
 
   useEffect(() => {
     if (!isLoaded && process.env.NEXT_PUBLIC_ENABLE_LEADERBOARD === "true") {
-      setLeaderboard(true);
+      // setLeaderboard(true);
       setIsLoaded(true);
     }
   }, []);
